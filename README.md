9d8dee62b5baa60e55523c180ee535b5b2a2b130
rikiTada <riki.tada777@gmail.com>
2024-04-30 14:31:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 開発メモ
+
+- `bun create vite`
+
+- install Tailwind with vite.
+ <https://tailwindcss.com/docs/guides/vite>
+
+- install shadcs/ui
+  <https://ui.shadcn.com/docs/installation/vite>
---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "preact/hooks";
+
+export function App() {
+  return (
+    <>
+      <div class="min-w-24 min-h-24">
+        <a
+          href="https://attendance.moneyforward.com/my_page"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MF出退勤
+        </a>
+      </div>
+    </>
+  );
+}
