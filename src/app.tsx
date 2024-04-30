3650b8525bb54ee0519f53568c66360086e393b7
rikiTada <riki.tada777@gmail.com>
2024-04-30 18:05:26 +0900

    Fix: version1.0.0

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,41 @@
-// import { useState } from "preact/hooks";
+import { Button, LinkButton, Title } from "./components";
 
-export function App() {
+export const App = () => {
+  const url = "https://attendance.moneyforward.com/my_page";
+
+  const createTab = (hash: string) => {
+    const createdURL = `${url}${hash}`;
+    chrome.tabs.create({ url: createdURL });
+  };
+
+  const buttons = [
+    { title: "出勤", location: "clock_in" },
+    { title: "休憩開始", location: "break" },
+    { title: "休憩終了", location: "returned" },
+    { title: "退勤", location: "clock_out" },
+  ];
+
   return (
-    <>
-      <div class="min-w-24 min-h-24">
-        <a
-          href="https://attendance.moneyforward.com/my_page"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MF出退勤
-        </a>
+    <div class="m-8">
+      <Title title="出勤・退勤" />
+
+      {buttons.map((button) => (
+        <div class="my-2">
+          <Button
+            title={button.title}
+            onClick={() => createTab(`#${button.location}`)}
+          />
+        </div>
+      ))}
+
+      <hr class="my-4" />
+
+      <div class="my-2">
+        <LinkButton
+          title="マネーフォワードで確認する"
+          onClick={() => chrome.tabs.create({ url })}
+        />
       </div>
-    </>
+    </div>
   );
-}
+};
